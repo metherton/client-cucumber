bbbfa9943996681173c2ae97677b811002df9225
metherton <martinetherton63@gmail.com>
2020-05-31 23:32:03 +0200

    generalized tests

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -17,9 +17,9 @@ public class StepDefinitions {
     private String today;
     private String actualAnswer;
 
-    @Given("today is Sunday")
-    public void today_is_Sunday() {
-        today = "Sunday";
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
     }
 
     @When("I ask whether it's Friday yet")
@@ -30,11 +30,6 @@ public class StepDefinitions {
     @Then("I should be told {string}")
     public void i_should_be_told(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
-    }
-
-    @Given("today is Friday")
-    public void today_is_Friday() {
-        today = "Friday";
     }
 
 }
