fb1110348d376526ba6997a77181e35cd7b4fbac
metherton <martinetherton63@gmail.com>
2020-05-31 23:19:53 +0200

    added tests for friday

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -6,25 +6,35 @@ import io.cucumber.java.en.When;
 
 import static org.junit.Assert.*;
 
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
+
 public class StepDefinitions {
+
+    private String today;
+    private String actualAnswer;
 
     @Given("today is Sunday")
     public void today_is_Sunday() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        today = "Sunday";
     }
 
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_Friday_yet() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        actualAnswer = IsItFriday.isItFriday(today);
     }
 
     @Then("I should be told {string}")
-    public void i_should_be_told(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
     }
 
+    @Given("today is Friday")
+    public void today_is_Friday() {
+        today = "Friday";
+    }
 
 }
